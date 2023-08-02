931559bb3fc76661335228d0ce5e87be2a25720d
Seyed Saeed Mohammadi <49374230+saeed090@users.noreply.github.com>
2023-08-02 07:17:03 +0330

    Create petros.py

---
+++ petros.py
@@ -0,0 +1,7 @@
+
+import os
+import webview
+
+#webview.create_window('پتروس آکادمی', 'https://apperio.ir/m/petrosacademy#/nav/signup221')
+webview.create_window('پتروس آکادمی', url='https://apperio.ir/m/petrosacademy#/nav/signup221',width=1900, height=1000 , resizable=True , fullscreen=True , min_size=(600, 1000))
+webview.start()
